d93d02679d600382b448c000452bff0196dd1404
unknown <dreeaam1993@gmail.com>
2026-03-17 20:25:43 +0400

    Session 3: Add Product interface, ProductsService, DI, and component communication

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,18 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  // --- الجزء الأول: ngIf ---
   isLoggedIn: boolean = false;
   studentName: string = 'Ahlam';
 
-  // --- الجزء الثاني: ngFor (Step 7) ---
   skills: string[] = ['Angular', 'TypeScript', 'HTML', 'CSS'];
 
-  // --- الجزء الثالث: ngSwitch (Step 11) ---
-  // جربي تغيير القيمة هنا بين 'admin' أو 'editor' أو 'viewer' لرؤية التغيير
+
   userRole: string = 'editor';
 
-  // دالة تبديل الدخول
   toggleLogin() {
     this.isLoggedIn = !this.isLoggedIn;
   }
